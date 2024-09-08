bc22bc1080da76ef6c51a338d97423a72e331a5a
Heryz1 <f.guibreteau@gmail.com>
2024-09-08 11:33:48 +0200

    a voir si il faut le garder

--- init.lua
+++ init.lua
@@ -16,6 +16,12 @@ require('mini.deps').setup({ path = { package = path_package } })
 -- startup and are optional.
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
+local available_lsp_servers = {
+    clangd = {},
+    pyright = {},
+    rust_analyzer = {},
+    lua_ls = {},
+}
 
 now(function()
     -- Use other plugins with `add()`. It ensures plugin is available in current
@@ -57,8 +63,8 @@ later(function() require('mason-lspconfig').setup(
 
 later(function() 
 require('mason-tool-installer').setup {
-    ensure_installed = { 'clangd', 'pyright', 'rust_analyzer', 'lua_ls',
-}, } end)
+    ensure_installed = available_lsp_servers
+} end)
 
 later(function() require('mini.ai').setup() end)        -- Add a/i text objects
 -- later(function() require('mini.align').setup() end)  -- Add alignment of data
@@ -242,76 +248,83 @@ vim.keymap.set('n', '{', '{zt', { desc = 'Jump to previous blank line, then set 
 -- Delete all buffer
 vim.api.nvim_create_user_command('Bda', function() vim.cmd('%bd') end, {})
 
--- later(function() require('nvim-lspconfig').setup(
--- ) end)
+-- later(function() require('lspconfig').setup() end)
 
--- later(function() require('nvim-lspconfig').setup(
-    -- -- -------------------------------------------------------------------------------------------------
-    -- -- LSP
-    -- -- -------------------------------------------------------------------------------------------------
-    -- --  This function gets run when an LSP attaches to a particular buffer.
-    -- --    That is to say, every time a new file is opened that is associated with
-    -- --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-    -- --    function will be executed to configure the current buffer
+-- -------------------------------------------------------------------------------------------------
+-- LSP
+-- -------------------------------------------------------------------------------------------------
+--  This function gets run when an LSP attaches to a particular buffer.
+--    That is to say, every time a new file is opened that is associated with
+--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+--    function will be executed to configure the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+    callback = function(event)      
+
+        -- We create a function that lets us more easily define mappings specific
+        -- for LSP related items. It sets the mode, buffer and description for us each time.
+        local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
+
+        -- Jump to the definition of the word under your cursor.
+        --  This is where a variable was first declared, or where a function is defined, etc.
+        --  To jump back, press <C-t>.
+        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+        -- Find references for the word under your cursor.
+        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+        -- Jump to the implementation of the word under your cursor.
+        --  Useful when your language has ways of declaring types without an actual implementation.
+        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+        -- Jump to the type of the word under your cursor.
+        --  Useful when you're not sure what type a variable is and you want to see
+        --  the definition of its *type*, not where it was *defined*.
+        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+        -- Fuzzy find all the symbols in your current document.
+        --  Symbols are things like variables, functions, types, etc.
+        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+        -- Fuzzy find all the symbols in your current workspace.
+        --  Similar to document symbols, except searches over your entire project.
+        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+        -- Rename the variable under your cursor.
+        --  Most Language Servers support renaming across files, etc.
+        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+        -- Execute a code action, usually your cursor needs to be on top of an error
+        -- or a suggestion from your LSP for this to activate.
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+
+        -- WARN: This is not Goto Definition, this is Goto Declaration.
+        --  For example, in C this would take you to the header.
+        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+        -- The following code creates a keymap to toggle inlay hints in your
+        -- code, if the language server you are using supports them
+        -- This may be unwanted, since they displace some of your code
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+            map('<leader>th', function()
+                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            end, '[T]oggle Inlay [H]ints')
+        end
+    end,
+})
+
+-- Configure the LSP servers here
+-- Add any additional override configuration in the following tables. Available keys are:
+-- - cmd (table): Override the default command used to start the server
+-- - filetypes (table): Override the default list of associated filetypes for the server
+-- - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+-- - settings (table): Override the default settings passed when initializing the server.
+-- See `:help lspconfig-all` for a list of all the pre-configured LSPs
+
     -- opts = function()
-    --     vim.api.nvim_create_autocmd('LspAttach', {
-    --         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-    --         callback = function(event)      
-    --
-    --             -- We create a function that lets us more easily define mappings specific
-    --             -- for LSP related items. It sets the mode, buffer and description for us each time.
-    --             local map = function(keys, func, desc, mode)
-    --                 mode = mode or 'n'
-    --                 vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-    --             end
-    --
-    --             -- Jump to the definition of the word under your cursor.
-    --             --  This is where a variable was first declared, or where a function is defined, etc.
-    --             --  To jump back, press <C-t>.
-    --             map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    --
-    --             -- Find references for the word under your cursor.
-    --             map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    --
-    --             -- Jump to the implementation of the word under your cursor.
-    --             --  Useful when your language has ways of declaring types without an actual implementation.
-    --             map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    --
-    --             -- Jump to the type of the word under your cursor.
-    --             --  Useful when you're not sure what type a variable is and you want to see
-    --             --  the definition of its *type*, not where it was *defined*.
-    --             map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-    --
-    --             -- Fuzzy find all the symbols in your current document.
-    --             --  Symbols are things like variables, functions, types, etc.
-    --             map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    --
-    --             -- Fuzzy find all the symbols in your current workspace.
-    --             --  Similar to document symbols, except searches over your entire project.
-    --             map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-    --
-    --             -- Rename the variable under your cursor.
-    --             --  Most Language Servers support renaming across files, etc.
-    --             map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    --
-    --             -- Execute a code action, usually your cursor needs to be on top of an error
-    --             -- or a suggestion from your LSP for this to activate.
-    --             map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-    --
-    --             -- WARN: This is not Goto Definition, this is Goto Declaration.
-    --             --  For example, in C this would take you to the header.
-    --             map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    --
-    --             -- The following code creates a keymap to toggle inlay hints in your
-    --             -- code, if the language server you are using supports them
-    --             -- This may be unwanted, since they displace some of your code
-    --             if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-    --                 map('<leader>th', function()
-    --                     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-    --                 end, '[T]oggle Inlay [H]ints')
-    --             end
-    --         end,
-    --     })
     --
     --     -- TODO seemes unnecessary with mini.completion 
     --     -- check if it is useless 
@@ -324,29 +337,9 @@ vim.api.nvim_create_user_command('Bda', function() vim.cmd('%bd') end, {})
     --     -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
     --
     --
-    --     -- Enable the following language servers
-    --     -- Add any additional override configuration in the following tables. Available keys are:
-    --     -- - cmd (table): Override the default command used to start the server
-    --     -- - filetypes (table): Override the default list of associated filetypes for the server
-    --     -- - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --     -- - settings (table): Override the default settings passed when initializing the server.
-    --     -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
-    --     --
-    --     -- Some languages (like typescript) have entire language plugins that can be useful:
-    --     --    https://github.com/pmizio/typescript-tools.nvim
-    --     -- For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-    --     local servers = {
-    --         clangd = {},
-    --         pyright = {},
-    --         rust_analyzer = {},
-    --         lua_ls = {},
-    --     }
     --
     -- end,
-
--- ) end)
-
-
+    --
 
 
 
